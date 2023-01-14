f856f3b2c4b5e153c183941c8ce826519b9227cc
Abraham Jaet <abrahamjaetb@gmail.com>
2023-01-14 08:54:44 -0700

    done and commented

--- recursive_Binary_Search.py
+++ recursive_Binary_Search.py
@@ -2,31 +2,36 @@ import random
 
 def recursive_Binary_Search(arr, target, low, high ): 
    
-    if high == low: 
+    if high == low: # check for end of search 
         if arr[high] == target:  
-            return high 
+            return high # == low 
         else: 
-            return -1
+            return -1 # not found 
     else: 
-        mid = (high + low) // 2 
+        mid = (high + low) // 2 # calc mid using floor ex. 7//2 -> 3
         
-        if arr[mid] == target: 
+        if arr[mid] == target: # target found 
             return mid 
         elif arr[mid] > target: 
             # need to go lower 
-            return recursive_Binary_Search(arr,target,low,mid-1)
+            high = mid - 1 # change the high value 
+            return recursive_Binary_Search(arr,target,low,high) 
         else: 
             # need to go higher
-            return recursive_Binary_Search(arr,target,mid+1,high)
+            low = mid +1 # change the low value 
+            return recursive_Binary_Search(arr,target,low,high) 
 
 
 
-arr = [1,2,3,4,5,6,7,8,9,10,12,14,16,17] 
-low = 0 
-high = len(arr) - 1
+# array to test
+arr = [1,2,3,4,5,6,7,8,9,10,12,14,16,17,20] 
+low = 0 # initialize low to 0 
+high = len(arr) - 1 # initialize high to max index 
+
+# for loop that checks for all numbers in the array 
 for i in arr: 
-    i = random.randint(1,100)
-    idx = recursive_Binary_Search(arr, i,low, high)
+    # i = random.randint(1,100) # Uncomment this line to look for random integers in the sequence 
+    idx = recursive_Binary_Search(arr, i,low, high) # function call 
     if idx == -1: 
         print('Target: ',i,' not found in sequence')
     else: 
